fee64264bcd7c0c6764bd1de3010884b6188051b
kbparagua <kb.paragua@gmail.com>
2026-04-11 11:40:22 +0800

    render products by categories

--- script.js
+++ script.js
@@ -11,25 +11,59 @@ document.addEventListener('DOMContentLoaded', async () => {
     const text = await response.text();
     const data = JSON.parse(text);
 
-    const products = data.products ? parseProducts(data.products) : [];
+    const products = data.products ? loadProducts(data.products) : [];
 
-    console.log(products);
     console.log(data);
 });
 
-function parseProducts(rawData) {
-    const products = [];
+function loadProducts(rawData) {
+  const categories = {};
+  const products = {};
 
-    for (let i = 0; i < rawData.length; i++) {
-        const product = {};
-        for (let j = 0; j < PRODUCT_COLUMNS.length; j++) {
-            product[PRODUCT_COLUMNS[j]] = rawData[i][j];
-        }
-
-        if (product.id == '') return products;
-
-        products.push(product);
+  for (let i = 0; i < rawData.length; i++) {
+    const product = {};
+    for (let j = 0; j < PRODUCT_COLUMNS.length; j++) {
+      product[PRODUCT_COLUMNS[j]] = rawData[i][j];
     }
 
-    return products;
+    if (product.id == '') break;
+
+    products[product.id] = product;
+
+    categories[product.category] = categories[product.category] || [];
+    categories[product.category].push(product);
+  }
+
+  console.log(categories);
+  renderCategories(categories);
 }
+
+function renderCategories(categories) {
+  for (const category in categories) {
+    const template = document.getElementById('js-category-template');
+    const categoryElement = template.content.cloneNode(true);
+
+    categoryElement.querySelector('.js-category-name').textContent = category;
+    
+    const productList = categoryElement.querySelector('.js-product-list');
+    const products = categories[category];
+    products.forEach(product => {
+      appendProductToList(product, productList);
+    });
+
+    document.getElementById('js-categories').appendChild(categoryElement);
+  };
+}
+
+function appendProductToList(product, list) {
+  const template = document.getElementById('js-product-template');
+  const productElement = template.content.cloneNode(true);
+
+  productElement.querySelector('.js-product-name').textContent = product.name;
+  productElement.querySelector('.js-product-description').textContent = product.description;
+  productElement.querySelector('.js-product-price').textContent = `P${product.price}`;
+  productElement.querySelector('.js-product-image').src = product.imageUrl;
+
+  list.appendChild(productElement);
+}
+
